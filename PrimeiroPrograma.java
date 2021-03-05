f99c983a1e9d439e9737f90fb9d22a08f62d37a4
Amanda Casagrande <200290123@estudantes.ips.pt>
2021-03-05 14:09:40 -0300

    Primeiro Commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva a descrição da classe PrimeiroPrograma aqui.
+ * 
+ * @author Amanda Casagrande 
+ * @version 05/03/2021
+ */
+public class PrimeiroPrograma
+{
+    public static void main(String[] args) {
+	System.out.println("Olá, Mundo!");
+    }
+
+}
